a86accf3d73b3026838fc017148bdd86b9f8c81c
Mara Schulke <mara@schulke.xyz>
2021-11-03 17:59:31 +0100

    Refactor the room system

--- signaling/src/rooms.rs
+++ signaling/src/rooms.rs
@@ -1,16 +1,41 @@
-use async_std::sync::{Arc, Mutex, MutexGuard};
-use std::collections::HashMap;
+use async_std::net::TcpStream;
+use std::collections::{HashMap, HashSet};
 use uuid::Uuid;
+use async_sub::Observable;
+use async_tungstenite::WebSocketStream;
+use serde::{Serialize, Deserialize};
+
+pub type RoomMap = HashMap<u8, Room>;
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Room {
+    creator: Uuid,
+    peers: HashSet<Uuid>,
+    negotiation: Observable<NegotiationState>
+}
+
+impl Room {
+    pub fn new(creator: Uuid) -> Room {
+        Room {
+            creator,
+            peers: {
+                let mut set = HashSet::new();
+                set.insert(creator);
+                set
+            },
+            negotiation: Observable::new(NegotiationState::Initial)
+        }
+    }
+
+    pub fn join(&mut self, peer: Uuid) {
+        self.peers.insert(peer);
+    }
+}
 
 #[derive(Clone, Debug)]
-pub struct RoomMap(Arc<Mutex<HashMap<Uuid, String>>>);
-
-impl<'a> RoomMap {
-    pub fn new() -> Self {
-        Self(Arc::new(Mutex::new(HashMap::new())))
-    }
-
-    pub async fn lock(&'a self) -> MutexGuard<'a, HashMap<Uuid, String>> {
-        self.0.lock().await
-    }
+pub enum NegotiationState {
+    Initial,
+    Offered(WebSocketStream<async_rustls::server::TlsStream<TcpStream>>, ()),
+    Answered,
+    Connected
 }
