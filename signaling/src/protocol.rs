658736a406460110fb5b8a81c7199ec7bd36d0bd
Mara Schulke <mara@schulke.xyz>
2021-11-03 17:59:53 +0100

    Start a dummy implementation of the signaling protocol

--- signaling/src/protocol.rs
+++ signaling/src/protocol.rs
@@ -1,33 +1,54 @@
 use anyhow::{bail, Context};
+use async_tungstenite::tungstenite::Message;
 use async_std::net::TcpStream;
+use async_rustls::server::TlsStream;
 use async_tungstenite::WebSocketStream;
 use serde::{Deserialize, Serialize};
-use futures::StreamExt;
+use futures::{SinkExt, StreamExt};
 use uuid::Uuid;
-use crate::rooms::RoomMap;
+use crate::rooms::Room;
+use crate::ServerState;
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type", content = "data", rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum PeerMessage {
-    Signup,
-    Connect(Uuid),
+    Create(()), // Create a room with an offer
+    Join(u8), // Join a room
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(tag = "type", content = "data", rename_all = "SCREAMING_SNAKE_CASE")]
 pub enum ServerMessage {
-    Ice(String),
-    Sdp(String),
-    Registered(Uuid)
+    Hello(Uuid),
+    Created(u8),
+
+    Joined(()), // joined and got an offer
+    Failed
+}
+
+impl From<ServerMessage> for Message {
+    fn from(server_msg: ServerMessage) -> Message {
+        Message::Binary(serde_json::to_vec(&server_msg).unwrap())
+    }
 }
 
 pub async fn handler(
-    mut websocket: WebSocketStream<TcpStream>,
-    rooms: RoomMap,
+    mut websocket: WebSocketStream<TlsStream<TcpStream>>,
+    state: ServerState
 ) -> anyhow::Result<()> {
-    use async_tungstenite::tungstenite::Message;
-
     log::info!("websocket connection established");
 
-    loop {
+    let uuid = Uuid::new_v4();
+
+    let mut lock = state.lock().await;
+    lock.1.insert(uuid);
+    drop(lock);
+
+    websocket.send(ServerMessage::Hello(uuid).into()).await?;
+
+    log::info!("registered peer {:?}", uuid);
+
+    'msg: loop {
         let websocket_msg = websocket
             .next()
             .await
@@ -44,15 +65,41 @@ pub async fn handler(
         };
 
         match message {
-            PeerMessage::Signup => {
-                let mut map = rooms.lock().await;
-                map.insert(Uuid::new_v4(), "this is a test".to_string());
-            },
-            PeerMessage::Connect(uuid) => {
-                log::info!("Connect this peer with the peer {:?}", uuid);
+            PeerMessage::Join(room_id) => {
+                let mut lock = state.lock().await;
+                let room = lock.0.get_mut(&room_id);
+                match room {
+                    Some(room) => {
+                        room.join(uuid);
+                        log::info!("(peer {}) joined {}", uuid, room_id);
+                        websocket.send(ServerMessage::Joined.into()).await?;
+                    }
+                    None => {
+                        log::error!("(peer {}) tried to join {} but it doesnt exist", uuid, room_id);
+                        websocket.send(ServerMessage::Failed.into()).await?;
+                    }
+                }
+            }
+            PeerMessage::Create => {
+                let mut lock = state.lock().await;
+
+                for room_id in 0..255 {
+                    if lock.0.contains_key(&room_id) {
+                        continue;
+                    }
+
+                    lock.0.insert(room_id, Room::new(uuid));
+                    websocket.send(ServerMessage::Created(room_id).into()).await?;
+
+                    log::info!("(peer {}) created {}", uuid, room_id);
+                    continue 'msg;
+                }
+
+                websocket.send(ServerMessage::Failed.into()).await?;
+                log::error!("(peer {}) tried to create room but all are used", uuid);
             }
         }
 
-        log::info!("session state: {:#?}", rooms);
+        log::info!("session state: {:#?}", state);
     }
 }
