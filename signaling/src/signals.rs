55e22a92dba69573bf2735361a14e7789963fad2
Mara Schulke <mara@schulke.xyz>
2021-11-03 17:57:29 +0100

    Add the available signal types

---
+++ signaling/src/signals.rs
@@ -0,0 +1,26 @@
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum IceOrSdp {
+    Ice(Ice),
+    Sdp(Sdp),
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Ice {
+    candidate: String,
+    #[serde(rename = "sdpMLineIndex")]
+    line_index: u32,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Sdp {
+    r#type: SdpType,
+    sdp: String,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize, Eq, PartialEq)]
+#[serde(rename_all = "camelCase")]
+pub enum SdpType {
+    Offer,
+    Answer,
+}
