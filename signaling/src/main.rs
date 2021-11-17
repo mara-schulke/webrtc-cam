35a1e8402007bd99fa14db92f26bbef1aecb7d2d
Mara Schulke <mara@schulke.xyz>
2021-11-17 19:58:13 +0100

    Support dynamically setting the environment

--- signaling/src/main.rs
+++ signaling/src/main.rs
@@ -1,11 +1,13 @@
 use anyhow::bail;
 use async_std::net::TcpListener;
-use async_std::task;
 use async_std::sync::{Arc, Mutex, MutexGuard};
+use async_std::task;
 use async_tungstenite::accept_async;
 use simple_logger::SimpleLogger;
 use std::collections::HashSet;
+use structopt::StructOpt;
 use uuid::Uuid;
+use std::str::FromStr;
 
 mod protocol;
 mod rooms;
@@ -15,13 +17,28 @@ mod signals;
 use rooms::RoomMap;
 
 #[derive(Clone, Debug)]
-#[allow(dead_code)]
 pub enum Environment {
     Production,
     Development
 }
 
-const ENVIRONMENT: Environment = Environment::Development;
+impl FromStr for Environment {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> anyhow::Result<Environment> {
+        match s {
+            "dev" | "development" => Ok(Environment::Development),
+            "prod" | "production" => Ok(Environment::Production),
+            _ => bail!("Unknown environment")
+        }
+    }
+}
+
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(short = "e", long = "env")]
+    environment: Environment
+}
 
 #[derive(Clone, Debug)]
 pub struct ServerState(Arc<Mutex<(RoomMap, HashSet<Uuid>)>>);
@@ -39,20 +56,31 @@ impl<'a> ServerState {
 #[async_std::main]
 async fn main() -> anyhow::Result<()> {
     SimpleLogger::new()
-        .with_level(log::LevelFilter::Info)
+        .with_level(log::LevelFilter::Debug)
         .init()
         .unwrap();
 
+    let Options { environment } = Options::from_args();
     let state = ServerState::new();
-    let listener = TcpListener::bind("127.0.0.1:8192").await?;
+    let listener = TcpListener::bind("0.0.0.0:8192").await?;
 
-    log::info!("running in mode {:?}", ENVIRONMENT);
-    log::info!("started listening on wss://127.0.0.1:8192");
+    log::info!("running in mode {:?}", environment);
+    log::info!("started listening on wss://0.0.0.0:8192");
 
-    let acceptor = match ENVIRONMENT {
+    let acceptor = match environment {
         Environment::Production => tls::create_acme_acceptor().await,
         Environment::Development => tls::create_self_signed_acceptor().await
     };
+
+    let log_state = state.clone();
+    task::spawn(async move {
+        loop {
+            let lock = log_state.0.lock().await;
+            log::info!("Server State: {:#?}", lock);
+            drop(lock);
+            task::sleep(std::time::Duration::from_millis(3000)).await;
+        }
+    });
 
     loop {
         match listener.accept().await {
