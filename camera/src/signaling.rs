6de92170128fc5f5ae8f99c0221bc0793a70ea89
Mara Schulke <mara@schulke.xyz>
2021-11-28 00:58:44 +0100

    Implement the camera signaling part

--- camera/src/signaling.rs
+++ camera/src/signaling.rs
@@ -1,7 +1,10 @@
+use uuid::Uuid;
+use anyhow::{anyhow, bail, Context};
 use async_tungstenite::WebSocketStream;
-use async_std::channel::{Sender, Receiver};
+use futures::{SinkExt, StreamExt};
 use url::Url;
 use crate::Environment;
+use signaling_types::rooms::RoomId;
 use signaling_types::protocol::{PeerMessage, ServerMessage};
 
 pub type WebSocket = WebSocketStream<async_rustls::TlsStream<async_std::net::TcpStream>>;
@@ -9,15 +12,53 @@ pub type WebSocket = WebSocketStream<async_rustls::TlsStream<async_std::net::Tcp
 const SIGNALING_SERVER_ADDRESS: &str = "wss://signaling.schulke.xyz:8192";
 const SIGNALING_SERVER_ADDRESS_LOCAL: &str = "wss://localhost:8192";
 
+pub async fn read_from_ws(websocket: &mut WebSocket) -> anyhow::Result<ServerMessage> {
+    use async_tungstenite::tungstenite::Message;
+
+    let message = loop {
+        match websocket.next().await {
+            None => bail!("websocket stream ended unexpectedly"),
+            Some(Ok(Message::Binary(json))) => break serde_json::from_slice(json.as_slice())?,
+            Some(Ok(Message::Text(json))) => break serde_json::from_str(json.as_str())?,
+            Some(Ok(Message::Ping(_))) | Some(Ok(Message::Pong(_))) => continue,
+            Some(Ok(Message::Close(_))) => bail!("websocket: received close"),
+            Some(Err(err)) => bail!("websocket stream error: {:?}", err),
+        }
+    };
+
+    Ok(message)
+}
+
+pub async fn write_to_ws(websocket: &mut WebSocket, msg: &PeerMessage) -> anyhow::Result<()> {
+    use async_tungstenite::tungstenite::Message;
+    let message = Message::Binary(serde_json::to_vec(msg)?);
+    websocket.send(message).await.with_context(|| anyhow!("Failed to send message {:#?}", msg))
+}
+
 pub async fn entry(environment: Environment) -> anyhow::Result<WebSocket> {
     log::info!("Starting signaling!");
 
-    let websocket = {
+    let mut websocket = {
         let url = Url::parse(SIGNALING_SERVER_ADDRESS_LOCAL)?;
         let tls_stream = tls::get_dangerous_tls_stream(&url).await?;
         let (connection, _) = async_tungstenite::client_async(url.to_string(), tls_stream).await?;
         connection
     };
+
+    let uuid = match read_from_ws(&mut websocket).await? {
+        ServerMessage::Hello(uuid) => uuid,
+        _ => bail!("Unexpected message from server")
+    };
+
+    log::info!("We have this uuid: {}", uuid.to_simple());
+
+    let room_id = RoomId::new(Uuid::from_slice(&[255; 16])?, Uuid::from_slice(&[255; 16])?);
+    write_to_ws(&mut websocket, &PeerMessage::JoinOrCreate(room_id)).await?;
+
+    match read_from_ws(&mut websocket).await? {
+        ServerMessage::Joined => log::info!("Joined room {}", room_id),
+        msg => bail!("Unexpected message from server: {:#?}", msg)
+    }
 
     Ok(websocket)
 }
